5df47cc1a06ff6041e50715d9f723bcb19652f0b
srilakshmi <dirisalasrilakshmi@gmail.com>
2024-02-21 15:13:09 +0530

    added

--- database/index.js
+++ database/index.js
@@ -1,8 +1,27 @@
+// const sqlite3 = require('sqlite3').verbose();
+
+// // Create a new database instance
+// const url=process.env.URL
+// const db = new sqlite3.Database(url, sqlite3.OPEN_READWRITE, (err) => {
+//   if (err) {
+//     console.error('Database Connection Failed! Bad Config:', err);
+//   } else {
+//     console.log('Connected to SQLite database');
+//   }
+// });
+
+// module.exports = {
+//   db
+// };
+
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+// Construct the file path using __dirname or process.cwd()
+const dbPath = path.join(__dirname, 'Chinook.db'); // Adjust the file name as needed
 
 // Create a new database instance
-const url=process.env.URL
-const db = new sqlite3.Database(url, sqlite3.OPEN_READWRITE, (err) => {
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error('Database Connection Failed! Bad Config:', err);
   } else {
@@ -13,3 +32,4 @@ const db = new sqlite3.Database(url, sqlite3.OPEN_READWRITE, (err) => {
 module.exports = {
   db
 };
+
